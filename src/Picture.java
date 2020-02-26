8ffeddb790e516fb7147a20ad93fe21640ab9665
Chris Urbano <ChrisUrbano1116@gmail.com>
2020-02-25 22:19:18 -0700

    a7

--- src/Picture.java
+++ src/Picture.java
@@ -135,18 +135,30 @@ public class Picture extends SimplePicture {
 		Pixel topPixel = null;
 		Pixel bottomPixel = null;
 		int height = pixels.length;
-		for (int row = 0; row < pixels.length; row++) {
-			for (int col = 0; col < width / 2; col++) {
-				rightPixel = pixels[row][col];
-				leftPixel = pixels[row][width - 1 - col];
-				leftPixel.setColor(rightPixel.getColor());
+		for (int row = 0; row < height / 2; row++) {
+			for (int col = 0; col < pixels[0].length / 2; col++) {
+				topPixel = pixels[row][col];
+				bottomPixel = pixels[height - 1 - col][col];
+				bottomPixel.setColor(topPixel.getColor());
 			}
 		}
 
-		
-		
 	}
+	public void mirrorHorizontalBotToTop() {
+		Pixel[][] pixels = this.getPixels2D();
+		Pixel topPixel = null;
+		Pixel bottomPixel = null;
+		int height = pixels.length;
+		for (int row = 0; row < height / 2; row++) {
+			for (int col = 0; col < pixels[0].length / 2; col++) {
+				topPixel = pixels[height * 2 - 1 - col][col];
+				bottomPixel = pixels[row][col];
+				topPixel.setColor(bottomPixel.getColor());
+			}
+		}
 
+	}
+	
 	/** Mirror just part of a picture of a temple */
 	public void mirrorTemple() {
 		int mirrorPoint = 276;
